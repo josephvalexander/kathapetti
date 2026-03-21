bc084c96941138bce41d9700acb70c94f3db4e0d
josephvalexander <88662204+josephvalexander@users.noreply.github.com>
2026-03-21 12:00:36 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // APP_VERSION: __VERSION__
 const CACHE = 'kathapetti-__VERSION__';
-const ASSETS = ['./','./index.html','./icon-192.svg','./icon-512.svg','./icon-maskable.svg'];
+const ASSETS = ['./','./index.html','./icon-192.svg','./icon-512.svg','./icon-maskable.svg','./manifest.json'];
 
 self.addEventListener('install', e => {
   self.skipWaiting();
@@ -9,11 +9,31 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', e =>
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+// Network first for HTML, cache first for everything else
+self.addEventListener('fetch', e => {
+  const isHTML = e.request.headers.get('accept')?.includes('text/html');
+  if(isHTML){
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
+
+// Tell all open tabs to reload when a new SW takes over
+self.addEventListener('message', e => {
+  if(e.data === 'skipWaiting') self.skipWaiting();
+});
